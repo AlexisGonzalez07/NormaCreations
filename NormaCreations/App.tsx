36b21f61b5beefde7aa1d4d6d378f48e08ad94d0
AlexisGonzalez07 <alexis.gonzalez07@gmail.com>
2023-04-19 18:44:12 -0500

    Updated Stack Navigator to include a header and cart icon in each screen

--- NormaCreations/App.tsx
+++ NormaCreations/App.tsx
@@ -2,49 +2,86 @@ import { StatusBar } from "expo-status-bar";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { RootStackParamList } from "./Navigation/types";
-import {lazy,Suspense} from 'react'
+import { lazy, Suspense } from "react";
 import { colors } from "./Components/colors";
 import { CartProvider } from "./CartContext";
+import CustomPressable from "./Components/Pressables/CustomPressable";
+import { Ionicons } from "@expo/vector-icons";
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-const LandingScreen = lazy(() => import("./Screens/LandingPage/LandingScreen"))
-const BalloonScreen = lazy(() => import("./Screens/BalloonsPage/BalloonScreen"))
-const DecorationsScreen = lazy(() => import("./Screens/DecorationsPage/DecorationsScreen"))
-const ShopScreen = lazy(() => import("./Screens/ShopPage/ShopScreen"))
-const ContactScreen = lazy(() => import("./Screens/ContactPage/ContactScreen"))
-
-
+const LandingScreen = lazy(() => import("./Screens/LandingPage/LandingScreen"));
+const BalloonScreen = lazy(
+  () => import("./Screens/BalloonsPage/BalloonScreen")
+);
+const DecorationsScreen = lazy(
+  () => import("./Screens/DecorationsPage/DecorationsScreen")
+);
+const ShopScreen = lazy(() => import("./Screens/ShopPage/ShopScreen"));
+const ContactScreen = lazy(() => import("./Screens/ContactPage/ContactScreen"));
 
 export default function App() {
   return (
     <CartProvider>
-    <Suspense fallback={<h1>In Progress..</h1>}>
-    <NavigationContainer>
-      <Stack.Navigator 
-      screenOptions={{
-        headerShown: false,
-        title: "Norma's Creations"
-      }}
-      initialRouteName="Shop"
-     >
-        <Stack.Screen name="LandingPage" component={LandingScreen}
-         />
-        <Stack.Screen name="Shop" component={ShopScreen} options={{
-          title: "Norma's Shop"
-        }}/>
-        <Stack.Screen name="Decorations" component={DecorationsScreen} 
-         options={{
-          title: "Norma's Decorations"
-        }}/>
-        <Stack.Screen name="Balloons" component={BalloonScreen}  options={{
-          title: "Norma's Balloons"
-        }}/>
-        <Stack.Screen name="Contact" component={ContactScreen}  options={{
-          title: "Contact Norma's Creations"
-        }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-    </Suspense>
+      <Suspense fallback={<h1>In Progress..</h1>}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              title: "Norma's Creations",
+              headerTitleAlign: 'left',
+              headerTintColor: 'black',
+              headerStyle: {
+                backgroundColor: colors.lightBackground
+              },
+              headerRight: () => (
+                <CustomPressable onPress={() => console.log("open the cart modal") } style={{backgroundColor: colors.green, width: 'auto', padding: 3, marginRight: 10}}>
+                <Ionicons name={'cart'} size={28} /></CustomPressable>
+              )
+            }}
+            initialRouteName="Shop"
+          >
+            <Stack.Screen
+              name="LandingPage"
+              component={LandingScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Shop"
+              component={ShopScreen}
+              options={{
+                title: "Norma's Shop",
+                headerLeft: null
+              }}
+            />
+            <Stack.Screen
+              name="Decorations"
+              component={DecorationsScreen}
+              options={{
+                title: "Norma's Decorations",
+                headerLeft: null
+              }}
+            />
+            <Stack.Screen
+              name="Balloons"
+              component={BalloonScreen}
+              options={{
+                title: "Norma's Balloons",
+                headerLeft: null
+              }}
+            />
+            <Stack.Screen
+              name="Contact"
+              component={ContactScreen}
+              options={{
+                title: "Contact Norma's Creations",
+                headerLeft: null
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Suspense>
     </CartProvider>
   );
 }
